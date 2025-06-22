992a7681c946cce516a0de7a4a202488accf5a99
Johnes Sombra <jhonesk5@gmail.com>
2025-06-21 23:40:11 -0500

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Controle de Farmácia</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Farmácia Control</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/"><i class="fas fa-home"></i> Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/vendas.php"><i class="fas fa-shopping-cart"></i> Vendas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/medicamentos.php"><i class="fas fa-pills"></i> Medicamentos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/laboratorios.php"><i class="fas fa-industry"></i> Laboratórios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/fornecedores.php"><i class="fas fa-truck"></i> Fornecedores</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
