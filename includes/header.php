58099776d80681d66f61c51db28cea14c3c806f0
Johnes Sombra <jhonesk5@gmail.com>
2025-06-22 00:31:06 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Controle de Farmácia</title>
+    <!-- jQuery primeiro -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Depois o Bootstrap CSS e JS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link href="/SistemaFarmaciaControl/css/style.css" rel="stylesheet">
     <link rel="shortcut icon" href="/SistemaFarmaciaControl/favicon.ico" type="image/x-icon">
