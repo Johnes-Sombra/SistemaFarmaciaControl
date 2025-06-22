acd862cdab12bcbf5f7dd06999e991ffb24c96ee
Johnes Sombra <jhonesk5@gmail.com>
2025-06-21 23:40:15 -0500

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-white mt-5 py-3">
+        <div class="container text-center">
+            <p class="mb-0">Sistema de Controle de Farmácia - Desenvolvido para fins educacionais</p>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="/js/scripts.js"></script>
+</body>
+</html>
